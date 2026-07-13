71f986e790389d063981da1f03a578f93d829c61
Lenovo <Lenovo@DESKTOP-3CFBP8V>
2026-07-13 11:54:03 +0300

    AUTHENTICATİON 2)Business katmanına IAuthService ve AuthManager eklendi .

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
